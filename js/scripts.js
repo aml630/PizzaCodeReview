81fc612621330c321cab6d562d1dfd338c94dbaa
aml630 <alex.m.larson@gmail.com>
2016-01-29 14:40:42 -0800

    finished address printout to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ function Pizza(toppings, pizzaSize) {
   this.pizzaSize = pizzaSize
   this.pizzaPrice = this.price(toppings, pizzaSize)
 }
+
 Pizza.prototype.price = function() {
   var pizzaPrice = 10;
   var toppingsNumber = this.toppings
@@ -20,29 +21,58 @@ Pizza.prototype.price = function() {
 
 function Order() {
   this.Pizzas = []
-  this.Salads = []
+  this.Addresses = []
 }
 
-Order.prototype.totalPrice = function () {
+Order.prototype.totalPrice = function() {
   var orderPrice = 0
-  for (var i = 0; i<this.Pizzas.length;i++) {
+  for (var i = 0; i < this.Pizzas.length; i++) {
     var individual = this.Pizzas[i]
-     orderPrice += this.Pizzas[i].pizzaPrice
+    orderPrice += this.Pizzas[i].pizzaPrice
   }
   return orderPrice;
 }
 
-Order.prototype.printOrder = function () {
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.printOut = function() {
+  var text = '<div class="new-address">' +
+    '<div class="form-group">' +
+    '<label for="new-street">Street</label>' +
+    '<input type="text" class="form-control new-street">' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="new-city">City</label>' +
+    '<input type="text" class="form-control new-city">' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="new-state">State</label>' +
+    '<input type="text" class="form-control new-state">' +
+    '<p class = "address2 btn btn-info">Enter Address</p>' +
+    '</div>' +
+    '</div>';
+  return text
+}
+
+Order.prototype.printOrder = function() {
   var text = ""
-  for (var i = 0; i<this.Pizzas.length; i++ ) {
-    text += "<li>Your "+ (i+1) + "st item has " + this.Pizzas[i].toppings + " toppings on a " + this.Pizzas[i].pizzaSize + " pizza! costing " +  this.Pizzas[i].pizzaPrice + "</li>"
+  for (var i = 0; i < this.Pizzas.length; i++) {
+    text += "<li>Your " + (i + 1) + "st item has " + this.Pizzas[i].toppings + " toppings on a " + this.Pizzas[i].pizzaSize + " pizza! costing $" + this.Pizzas[i].pizzaPrice + "</li>"
   }
-  text += "your total order price will be " + this.totalPrice()
+  text += "<p class = 'totalOrder'>your total order price will be $" + this.totalPrice() + "</p>"
+  for (var i = 0; i < this.Addresses.length; i++) {
+    text += "<p> your order will be delivered to " + this.Addresses[0].street + " " + this.Addresses[0].city + " " + this.Addresses[0].state + "</p>"
+  };
   return text;
 }
 
 $(document).ready(function() {
   var newOrder = new Order()
+  var newAddress = new Address()
   var toppingsSelected = 0
   $(".btn-primary").click(function() {
     if ($(this).hasClass("clicked") === true) {
@@ -52,26 +82,34 @@ $(document).ready(function() {
     toppingsSelected++
     $("#toppingsNum").text(toppingsSelected)
   });
-
+  $(".address").click(function() {
+    $(".addressprint").append(newAddress.printOut())
+    $(".address").hide();
+    $(".address2").click(function() {
+      var street = $(".new-street").val()
+      var city = $(".new-city").val()
+      var state = $(".new-state").val()
+      newAddress.street = street
+      newAddress.city = city
+      newAddress.state = state
+      newOrder.Addresses.push(newAddress)
+      console.log(newOrder)
+    })
+  })
   $("form").submit(function(event) {
     var pizzaSize = $("#sizeList").val()
     var toppings = toppingsSelected
-
     var newPizza = new Pizza(toppings, pizzaSize);
     var finalPrice = newPizza.price(toppings, pizzaSize)
     $("#finalPrice").text(newPizza.price(toppings, pizzaSize))
-      newOrder.Pizzas.push(newPizza);
-      console.log(newOrder)
+    newOrder.Pizzas.push(newPizza);
+    console.log(newOrder)
     event.preventDefault()
     $(".pizzaPrintout").text("")
     $(".pizzaPrintout").append("<li>you've selected " + newPizza.toppings + " toppings on a " + newPizza.pizzaSize + " pizza! Sounds delicous!</li>")
   })
-
-
-  $(".viewOrder").click(function () {
+  $(".viewOrder").click(function() {
     $(".orderDetails").text("")
-      $(".orderDetails").append(newOrder.printOrder())
-
+    $(".orderDetails").append(newOrder.printOrder())
   })
-
 });
--- spec/specs.js
+++ spec/specs.js
@@ -11,31 +11,34 @@ describe('Pizza', function() {
     expect(newPizza.price()).to.equal(19);
   });
 });
-
-describe('Order', function () {
-  it("Will have empty array so store food objects in", function () {
-  var newOrder = new Order()
-  expect(newOrder.Pizzas).to.eql([])
-  expect(newOrder.Salads).to.eql([])
+describe('Order', function() {
+  it("Will have empty array so store food objects in and empty string to store address object", function() {
+    var newOrder = new Order()
+    expect(newOrder.Pizzas).to.eql([])
+    expect(newOrder.Addresses).to.eql([])
   })
-
-
-  it("Will add up all the prices of all the Pizzas ordered", function () {
-  var pizza1 = new Pizza(2, "large")
-  var pizza2 = new Pizza(3, "medium")
-  var newOrder = new Order()
-  newOrder.Pizzas.push(pizza1)
-  newOrder.Pizzas.push(pizza2)
-  expect(newOrder.totalPrice()).to.eql(43)
+  it("Will add up all the prices of all the Pizzas ordered", function() {
+    var pizza1 = new Pizza(2, "large")
+    var pizza2 = new Pizza(3, "medium")
+    var newOrder = new Order()
+    newOrder.Pizzas.push(pizza1)
+    newOrder.Pizzas.push(pizza2)
+    expect(newOrder.totalPrice()).to.eql(43)
   })
-
-
-it("Will print the full order", function () {
-var pizza1 = new Pizza(2, "large")
-var pizza2 = new Pizza(3, "medium")
-var newOrder = new Order()
-newOrder.Pizzas.push(pizza1)
-newOrder.Pizzas.push(pizza2)
-expect(newOrder.printOrder()).to.eql("<li>Your 1st item has 2 toppings on a large pizza! costing 24</li><li>Your 2st item has 3 toppings on a medium pizza! costing 19</li>your total order price will be 43")
+  it("Will print the full order", function() {
+    var pizza1 = new Pizza(2, "large")
+    var pizza2 = new Pizza(3, "medium")
+    var newOrder = new Order()
+    newOrder.Pizzas.push(pizza1)
+    newOrder.Pizzas.push(pizza2)
+    expect(newOrder.printOrder()).to.eql("<li>Your 1st item has 2 toppings on a large pizza! costing 24</li><li>Your 2st item has 3 toppings on a medium pizza! costing 19</li><p class = \'totalOrder\'>your total order price will be 43</p>")
+  })
 })
-})
+describe("Address", function() {
+  it("Will allow users to enter an address into the order form", function() {
+    var newAddress = new Address("123", "portland", "OR")
+    expect(newAddress.street).to.equal("123")
+    expect(newAddress.city).to.equal("portland")
+    expect(newAddress.state).to.equal("OR")
+  });
+});
