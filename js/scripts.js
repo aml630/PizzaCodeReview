5f6cb1f1f0f5dbeda5cdc81bddc5dab09cac64ea
aml630 <alex.m.larson@gmail.com>
2016-01-29 08:56:28 -0800

    object spec passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,41 @@
+function Pizza (toppings, pizzaSize) {
+  this.toppings = toppings;
+  this.pizzaSize = pizzaSize
+}
 
+Pizza.prototype.price = function () {
+var pizzaPrice = 10;
+var toppingsNumber = this.toppings.length
 
+if(this.pizzaSize === "small") {
+  pizzaPrice = pizzaPrice -5
+}else if (this.pizzaSize === "medium") {
+  pizzaPrice = pizzaPrice +3
+}else {
+  pizzaPrice = pizzaPrice +10
+}
 
+var toppingsPrice = toppingsNumber *2
 
+pizzaPrice = pizzaPrice + toppingsPrice;
 
+return pizzaPrice
 
-
-
-
-
-
+}
 
 
 
 $(document).ready(function() {
+$(".btn-primary").click(function() {
+  $(this).addClass("clicked")
+
+$("form").submit(function (event) {
+
+  event.preventDefault()
+
+
+})
+})
 
 
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,17 @@
-
 describe('Pizza', function() {
+  it("will have both toppings and size properties", function () {
+    var newPizza = new Pizza(["peppers", "onions"], "large")
+    expect(newPizza.toppings).to.eql(["peppers", "onions"])
+    expect(newPizza.pizzaSize).to.equal("large")
+  });
+
+  it("will allow price adjustments based on toppings and sizes", function () {
+    var newPizza = new Pizza(["peppers", "onions"], "large")
+    expect(newPizza.price()).to.equal(24);
+    var newPizza = new Pizza(["peppers", "onions", "pepperoni"], "medium")
+    expect(newPizza.price()).to.equal(19);
+  });
+
 
 
 
